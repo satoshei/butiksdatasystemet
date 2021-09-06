f3503a38a42c8f6591759a19166c0aedb6611c9a
satoshei <90180324+satoshei@users.noreply.github.com>
2021-09-06 10:00:15 +0200

    Add files via upload

---
+++ Databutiksystem.py
@@ -0,0 +1,13 @@
+finished = False
+while not finished:
+    kassa = input("Tryck enter för att öppna kassa!")
+    if kassa = " ":
+        print("KASSA")
+        print("1. Ny Kund")
+        print("0. Avsluta")
+
+    selection = input("Välj ett alternativ 1 eller 2")
+
+    try:
+        if selection == "1":
+            
